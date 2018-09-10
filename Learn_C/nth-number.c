0e84634fb594a2ab7ae4e2711cf7dddf473ee2d5
black.dragon74 <nickk.2974@gmail.com>
2018-09-10 17:31:42 +0530

    Add program to select n-th term of an integer
    Signed-off-by: black.dragon74 <nickk.2974@gmail.com>

---
+++ Learn_C/nth-number.c
@@ -0,0 +1,24 @@
+// Code is poetry
+// Created by black.dragon74
+
+#include <stdio.h>
+
+int main(){
+    
+    // Define an integer
+    int a = 1234567;
+    
+    /**
+     * a % 10 will always print last number.
+     * (a%100)/10 will print second last.
+     * (a%1000)/100 will print third last.
+     * (a%10000)/1000 will print fourth last.
+     * (a%100000)/10000 will print 1st number.
+     **/
+    
+    int toPrint = (a%100000)/10000; // will print the first number
+    
+    printf("The selected number is: %d\n", toPrint);
+    
+    return 1;
+}
