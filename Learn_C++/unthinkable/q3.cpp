56db2c3be147b86edeb591cd3c01799eda53bf17
black.dragon74 <nickk.2974@gmail.com>
2020-11-17 16:32:40 +0530

    Update q3
    Signed-off-by: black.dragon74 <nickk.2974@gmail.com>

--- Learn_C++/unthinkable/q3.cpp
+++ Learn_C++/unthinkable/q3.cpp
@@ -32,53 +32,32 @@ bool vowelsInAscending(const string &word) {
     return true;
 }
 
-void numOfDuplicateWords(const string &sentence, vector<string> &vec, vector<int> &countVec) {
-    char wordStore[100][100];        // Barbaric, IK, but that is what competitve programmig is.
-    memset(wordStore, 0, sizeof(wordStore));    // Zeroing the array before use is needed on some systems
+void numOfDuplicateWords(const string &sentence, map<string, int> &dict) {
+    istringstream stringStream(sentence);
 
-    int rowIterator = 0, colIterator = 0, i, length, count;
-    for (i = 0; sentence[i] != '\0'; i++) {
-        if (sentence[i] != ' ' && sentence[i] != '\0' && sentence[i] != '.') {
-            wordStore[rowIterator][colIterator++] = static_cast<char>(tolower(sentence[i]));
+    do {
+        string currWord;
+        stringStream >> currWord;
+        transform(currWord.begin(), currWord.end(), currWord.begin(), ::tolower);
+
+        if (dict.find(currWord) == dict.end()) {
+            dict.insert({currWord, 1});
         } else {
-            wordStore[rowIterator++][colIterator] = '\0';
-            colIterator = 0;
-        }
-    }
-    // Add 1 to the length as arrays start from 0
-    length = i + 1;
-
-    for (i = 0; i < length; i++) {
-        count = 1;
-        for (int j = i + 1; j < length; j++) {
-            if (!strcmp(wordStore[i], wordStore[j]) && strcmp(wordStore[j], "0") != 0) {
-                count++;
-                strcpy(wordStore[j], "0");
-            }
+            dict[currWord]++;
         }
 
-        if (count > 1) {
-            auto repeatedWord = wordStore[i];
-
-            if (strcmp(repeatedWord, "") != 0) {
-                vec.emplace_back(repeatedWord);
-                countVec.emplace_back(count);
-            }
-        }
-    }
+    } while (stringStream);
 }
 
 int main(int argc, char **argv) {
     string sample = "They don't know that we know they know we know.";
-    vector<string> strVec;
-    vector<int> countVec;
+    map<string, int> dict;
 
-    numOfDuplicateWords(sample, strVec, countVec);
+    numOfDuplicateWords(sample, dict);
 
-    for (int i = 0; i < strVec.size(); ++i) {
-        auto word = strVec[i];
-        if (vowelsInAscending(word)) {
-            cout << word << " " << countVec[i] << " ";
+    for (const auto &elem: dict) {
+        if (vowelsInAscending(elem.first) && elem.second > 1) {
+            cout << elem.first << " " << elem.second << " " << endl;
         }
     }
 }
