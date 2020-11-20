f5105484a777155e2c4fb61fde5270d149ca9937
black.dragon74 <nickk.2974@gmail.com>
2020-11-20 18:50:06 +0530

    Update questions
    Signed-off-by: black.dragon74 <nickk.2974@gmail.com>

--- Learn_CPP/hackerrank-ds/easy/linked_lists.cpp
+++ Learn_CPP/hackerrank-ds/easy/linked_lists.cpp
@@ -47,6 +47,11 @@ public:
         cout << endl;
     }
 
+    void deleteAllNodes() {
+        while (!empty())
+            popBack();
+    }
+
     void push(T elem) {
         auto *ptr = head;
         auto *tmp = new SinglyLinkedListNode<T>(elem);
@@ -261,14 +266,37 @@ public:
     bool empty() { return numElements == 0; };
 };
 
+int binarySearch(const vector<int> &arr, int x) {
+    int lb = 0;
+    int rb = static_cast<int>(arr.size() - 1);
+
+    while (lb <= rb) {
+        int mid = lb + (rb - lb) / 2;
+
+        if (arr[mid] == x)
+            return mid;
+
+        if (arr[mid] < x)
+            lb = mid + 1;
+        else
+            rb = mid -1;
+    }
+
+    return -1;
+}
+
 int main(int argc, char **argv) {
     auto myList = SinglyLinkedList<int>();
 
-    for (int i = 1; i <= 20; ++i) {
-        myList.push(i);
-    }
 
-    assert(myList.getValueAtTailIndex(0) == 20);
+
+//    int arr[] = {2, 3, 4, 10, 40};
+//    int x = 40;
+//    int n = sizeof(arr) / sizeof(arr[0]);
+//    int result = binarySearch(arr, 0, n - 1, x);
+//    (result == -1) ? cout << "Element is not present in array"
+//                   : cout << "Element is present at index " << result;
+
 
 //    myList.print();
 //    myList.insertValueAtIndex(1, 74);
